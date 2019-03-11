4014e1bf2e799f10a4e74e35f4fc2dbe300a2e3d
Sulka Haro <sulka@sulka.net>
2019-03-11 14:25:46 +0200

    Added HIDE_LOGIN env variable to hide login button in production while service is being developed

--- nsfi.js
+++ nsfi.js
@@ -7,8 +7,12 @@ const axios = require('axios');
 
 const Mongo = require('./lib/Mongo.js')();
 
-const { decorateApp } = require('@awaitjs/express');
-const { wrap } = require('@awaitjs/express');
+const {
+   decorateApp
+} = require('@awaitjs/express');
+const {
+   wrap
+} = require('@awaitjs/express');
 const app = decorateApp(express());
 
 const bodyParser = require('body-parser');
@@ -22,28 +26,28 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // !!!IMPORTANT: place this before static or similar middleware
-app.use('/public',expressmarkdown({
-    directory: path.join(__dirname,'/public'),
-    caseSensitive: app.get('case sensitive routing'),
-    view: 'markdown',
-    includerawtext: false,
-    loadepiceditor: false,
-    marked: {
-        renderer: new marked.Renderer(),
-        gfm: true,
-        tables: true,
-        breaks: false,
-        pedantic: false,
-        sanitize: true,
-        smartLists: true,
-        smartypants: false,
-        highlight: function (code) {
-            return highlightjs.highlightAuto(code).value;
-        }
-    },
-    context: {
-        title: 'Nightscout.fi'
-    }
+app.use('/public', expressmarkdown({
+   directory: path.join(__dirname, '/public')
+   , caseSensitive: app.get('case sensitive routing')
+   , view: 'markdown'
+   , includerawtext: false
+   , loadepiceditor: false
+   , marked: {
+      renderer: new marked.Renderer()
+      , gfm: true
+      , tables: true
+      , breaks: false
+      , pedantic: false
+      , sanitize: true
+      , smartLists: true
+      , smartypants: false
+      , highlight: function (code) {
+         return highlightjs.highlightAuto(code).value;
+      }
+   }
+   , context: {
+      title: 'Nightscout.fi'
+   }
 }));
 
 /*
@@ -54,9 +58,9 @@ app.use(require('express-markdown-reloaded')({
 */
 // cookieSession config
 app.use(cookieSession({
-    maxAge: 60 * 60 * 1000, //One hour
-    keys: [env.session_key], // Key used to verify the session data, set this for production
-    httpOnly: true
+   maxAge: 60 * 60 * 1000, //One hour
+   keys: [env.session_key], // Key used to verify the session data, set this for production
+   httpOnly: true
 }));
 
 app.use(passport.initialize()); // Used to initialize passport
@@ -66,56 +70,61 @@ passport.use(env.PassportStrategy);
 
 // Used to stuff a piece of information into a cookie
 passport.serializeUser((user, done) => {
-    done(null, user);
+   done(null, user);
 });
 
 // Used to decode the received cookie and persist session
 passport.deserializeUser((user, done) => {
-    done(null, user);
+   done(null, user);
 });
 
 // Middleware to check if the user is authenticated
-async function isUserAuthenticated(req, res, next) {
-    if (req.user) {
-        next();
-    } else {
-        res.redirect('/');
-    }
+async function isUserAuthenticated (req, res, next) {
+   if (req.user) {
+      next();
+   } else {
+      res.redirect('/');
+   }
 }
 
 
 // USER FACING URLS
 
 app.getAsync('/', async (req, res) => {
-//    console.log(req.headers);
-    res.render('index.ejs');
+   let pageEnv = {
+      hideLogin: env.hideLogin
+   };
+   res.render('index.ejs', {pageEnv: pageEnv});
 });
 
 app.getAsync('/loggedin', isUserAuthenticated, async function (req, res) {
-    let user = await env.userProvider.findUserById(req.user.userid);
+   let user = await env.userProvider.findUserById(req.user.userid);
 
-    let pageEnv = {
-        apiURL: env.apiURL
-    };
+   let pageEnv = {
+      apiURL: env.apiURL
+   };
 
-    res.render('secret.ejs', {user: user, pageEnv: pageEnv});
+   res.render('secret.ejs', {
+      user: user
+      , pageEnv: pageEnv
+   });
 });
 
 // Logout route
 app.get('/logout', (req, res) => {
-    req.logout(); 
-    res.render('loggedout.ejs');
+   req.logout();
+   res.render('loggedout.ejs');
 });
 
 // OAUTH
 
 app.get('/auth/kanta', passport.authenticate('oauth2', {
-    state: env.randomString(), // TODO actually store this in session to validate it
-    scope: ['offline_access', 'patient/Observation.read', 'patient/MedicationAdministration.read', 'patient/Observation.write', 'patient/MedicationAdministration.write' ] // Used to specify the required data
+   state: env.randomString(), // TODO actually store this in session to validate it
+   scope: ['offline_access', 'patient/Observation.read', 'patient/MedicationAdministration.read', 'patient/Observation.write', 'patient/MedicationAdministration.write'] // Used to specify the required data
 }));
 
 app.get('/auth/kanta/callback', passport.authenticate('oauth2'), function (req, res) {
-    res.redirect('/loggedin');
+   res.redirect('/loggedin');
 });
 
 let NSRestService = require('./lib/NSRESTService')(env);
@@ -131,5 +140,5 @@ app.use('/tpdata', TidepoolRESTService.dataApp);
 console.log('TidepoolRESTService started');
 
 app.listen(process.env.PORT, () => {
-    console.log('Server Started!');
+   console.log('Server Started!');
 });
