0460ed0a8696433f4497cfa3a8b086151c518880
Mikael Rinnetmäki <mikael@sensotrend.com>
2019-04-14 07:59:13 +0300

    Use shared description in eula

--- src/Eula/Eula.js
+++ src/Eula/Eula.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import Description from '../Instructions/Description';
+
 class Eula extends Component {
   render() {
     return (
       <div id='eula'>
+        <Description />
         <h2>Käyttöehdot</h2>
       </div>
     );
