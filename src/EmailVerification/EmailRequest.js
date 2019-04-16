10998b9b8b0c45e5c67268f624f1e63c707a6595
Mikael Rinnetmäki <mikael@sensotrend.com>
2019-04-16 10:43:18 +0300

    Start working on email confirmation

---
+++ src/EmailVerification/EmailRequest.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class EmailRequest extends Component {
+  render() {
+    return (
+      <div id="registration">
+        <h2>Rekisteröityminen</h2>
+        <p>Tarvitsemme sähköpostiosoitteesi voidaksemme tiedottaa kriittisistä vikatilanteista palvelussa.</p>
+        <p>Voit halutessasi saada sähköpostiosoitteeseesi myös tietoja palvelun vähemmän kriittisistä virhetilanteista.</p>
+        <p>Voit myös ilmaista halusi osallistua palvelun jatkokehitykseen.</p>
+        <p><a href="privacy">Tietosuojaseloste</a> kertoo tarkemmin tietojesi käytöstä.</p>
+        <form>
+          <div>
+            <input type="email" id="email" placeholder="sahkopostiosoite@palvelin.com"></input>
+          </div>
+          <div className="checkbox">
+            <input type="checkbox" id="notifications"></input>
+            <label for="notifications">Tahdon saada sähköpostiini tietoja palvelun vikatilanteista (esimerkiksi verkkoyhteyden tilapäinen katkeaminen).</label>
+          </div>
+          <div className="checkbox">
+            <input type="checkbox" id="development"></input>
+            <label for="development">Minulle saa lähettää viestejä ja kysymyksiä liittyen palvelun jatkokehitykseen</label>
+          </div>
+          <div>
+            <button className="large primary button-success" type="submit">Lähetä!</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EmailRequest;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -6,13 +6,13 @@ import {
   Switch,
 } from 'react-router-dom';
 
-import { server } from '../Api/Api';
 import Eula from '../Eula/Eula';
 import Instructions from '../Instructions/Instructions';
 import Index from '../Index/Index';
 import Logout from '../Logout/Logout';
 import Privacy from '../Privacy/Privacy';
 import Support from '../Support/Support';
+import EmailRequest from '../EmailVerification/EmailRequest';
 
 const base = process.env.PUBLIC_URL;
 const supportsHistory = 'pushState' in window.history;
@@ -97,18 +97,19 @@ class Routes extends Component {
         />
         <React.Fragment>
           <Switch>
+            <Route path="/eula" component={Eula} />
             <Route
               path="/index"
               render={props => <Index {...props} config={config} />}
             />
-            <Route path="/eula" component={Eula} />
-            <Route path="/privacy" component={Privacy} />
             <Route
               path="/instructions"
               render={props => <Instructions {...props} config={config} />}
             />
-            <Route path="/support" component={Support} />
             <Route path="/logout" component={Logout} />
+            <Route path="/privacy" component={Privacy} />
+            <Route path="/registration" component={EmailRequest} />
+            <Route path="/support" component={Support} />
             <Redirect from="/" to="/index" />
           </Switch>
         </React.Fragment>
