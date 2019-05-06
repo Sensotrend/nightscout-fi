5f8f83106146fa99060a375fd23217ac78199b4c
Sulka Haro <sulka@sulka.net>
2019-05-06 10:53:39 +0300

    Upload sample data to sandbox rather than using hardcoded patient ID

--- tests/nsrest.test.js
+++ tests/nsrest.test.js
@@ -1,57 +1,86 @@
 import nsfi from '../nsfi.js';
 
 import request from 'supertest';
+import uuidv4 from 'uuid/v4';
+import _FHIRClient from '../lib/FHIRClient';
+import { exist } from '../node_modules/should/should.js';
 
-let env = nsfi.env;
-let Auth = env.userProvider;
+import * as _sampleData from './fhir_data.json';
+const sampleData = _sampleData.default;
 
-let siteid = 'foo';
-let pw = 'bar';
+const env = nsfi.env;
+const Auth = env.userProvider;
+const siteid = 'foo';
+const pw = 'bar';
 
-describe('NS_REST_API', function () {
+const fhirserver = "http://hapi.fhir.org/baseDstu3";
+const FHIRClient = _FHIRClient(fhirserver);
 
-   it('should provide the /entries API', async function () {
+const UUID = uuidv4();
 
-      const u = await Auth.createUser('1710136', siteid, pw, new Date()); // sub, access_token, refresh_token,token_expiry_date
+const testPatient = {
+   "resourceType": "Patient",
+   "text": {
+      "status": "generated",
+      "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"><table class=\"hapiPropertyTable\"><tbody><tr><td>Identifier</td><td>urn:uuid:" + UUID + "</td></tr></tbody></table></div>"
+   },
+   "identifier": [
+      {
+         "system": "urn:ietf:rfc:3986",
+         "value": "urn:uuid:" + UUID
+     }
+   ]
+};
+
+let patient;
+
+describe('NS_REST_API & FHIRClient test', function () {
+
+   it('should create a sample patient and data to FHIR sandbox', async function () {
+      try {
+         console.log('CREATING PATIENT');
+         const results = await FHIRClient.createRecords(testPatient);
+         patient = results.records[0];
+
+         const patientRef = 'Patient/' + patient.id;
+
+         console.log('sampledata', sampleData);
+
+         sampleData.forEach(element => {
+            element.subject.reference = patientRef;
+            element.performer[0].reference = patientRef;
+         });
+
+         console.log('CREATING SAMPLE DATA');
+         const results2 = await FHIRClient.createRecords(sampleData);
+
+         console.log('results2', results2);
+      } catch (error) {
+         console.error(error);
+         false.should.equal(true);
+      }
+   });
+
+   it('should provide the /entries API to load sample data', async function () {
+
+      const u = await Auth.createUser(patient.id, siteid, pw, new Date()); // sub, access_token, refresh_token,token_expiry_date
 
       console.log('User for ENTRIES API TEST', u);
 
-      /*
-
-      {
-  "resourceType": "Patient",
-  "id": "1710136",
-  "meta": {
-    "versionId": "1",
-    "lastUpdated": "2019-04-02T11:05:22.302+00:00"
-  },
-  "text": {
-    "status": "generated",
-    "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"><table class=\"hapiPropertyTable\"><tbody><tr><td>Identifier</td><td>urn:uuid:27e91df0-5537-11e9-8c32-07261c8b74e5</td></tr></tbody></table></div>"
-  },
-  "identifier": [
-    {
-      "system": "urn:ietf:rfc:3986",
-      "value": "urn:uuid:27e91df0-5537-11e9-8c32-07261c8b74e5"
-    }
-  ]
-}
-*/
-
       let ns_sample = [{
-         "_id": "5c655105763fe276981ff0c2"
-         , "device": "xDrip-DexcomG5"
-         , "date": 1550143850509
-         , "dateString": "2019-02-14T13:30:50.509+0200"
-         , "sgv": 177
-         , "delta": 1.5
-         , "direction": "Flat"
-         , "type": "sgv"
-         , "filtered": 195071.0394182456
-         , "unfiltered": 196842.65552921052
-         , "rssi": 100
-         , "noise": 1
-         , "sysTime": "2019-02-14T13:30:50.509+0200"
+         "_id": "5c655105763fe276981ff0c2",
+         "device": "xDrip-DexcomG5",
+         "date": 1550143850509,
+         "dateString": "2019-02-14T13:30:50.509+0200",
+         "sgv": 177,
+         "delta": 1.5,
+         "direction": "Flat",
+         "type": "sgv",
+         "filtered": 195071.0394182456,
+         "unfiltered": 196842.65552921052,
+         "rssi": 100,
+         "noise": 1,
+         "sysTime": "2019-02-14T13:30:50.509+0200"
       }];
 
       await request(nsfi)
@@ -75,14 +104,14 @@ describe('NS_REST_API', function () {
 
    it('should provide the /treatments API', async function () {
 
-      const u = await Auth.createUser('1710136', siteid, pw, new Date()); // sub, access_token, refresh_token,token_expiry_date
+      const u = await Auth.createUser(patient.id, siteid, pw, new Date()); // sub, access_token, refresh_token,token_expiry_date
 
       let ns_sample = [{
-         "_id": "5c655105763fe276981ff0c2"
-         , "device": "MDT-554"
-         , "date": 1550143850509
-         , "carbs": 15
-         , "created_at": "2019-04-01T11:21:28+03:00"
+         "_id": "5c655105763fe276981ff0c2",
+         "device": "MDT-554",
+         "date": 1550143850509,
+         "carbs": 15,
+         "created_at": "2019-04-01T11:21:28+03:00"
    }];
 
       await request(nsfi)
