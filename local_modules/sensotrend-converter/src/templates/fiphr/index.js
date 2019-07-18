fb939b45a717a1f21fb0b10e33ec0cb535d2b9cf
Mikael Rinnetmäki <mikael@sensotrend.com>
2019-07-18 15:22:40 +0300

    Safeguard for missing value

--- local_modules/sensotrend-converter/src/templates/fiphr/index.js
+++ local_modules/sensotrend-converter/src/templates/fiphr/index.js
@@ -177,7 +177,7 @@ export class FIPHRDataProcessor extends DataFormatConverter {
             entry.deltaMmol = entry.delta;
          }
       }
-      narrative += `Tulos: ${entry.valueMmol.toFixed(1)} mmol/l<br />`;
+      narrative += `Tulos: ${entry.valueMmol ? entry.valueMmol.toFixed(1) : '?'} mmol/l<br />`;
       if (!isNaN(entry.deltaMmol)) {
          narrative += `Muutos: ${entry.deltaMmol.toFixed(2)} mmol/l`;
       }
