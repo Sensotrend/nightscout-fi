4ca9e2449799b5a2d4a079f09e759ede8323e3db
Mikael Rinnetmäki <mikael@sensotrend.com>
2019-07-18 18:50:50 +0300

    Modify text narrative
    Fix value precision (delta can be 2 decimals for both units).
    Omit direction and noise for now.

--- local_modules/sensotrend-converter/src/templates/fiphr/index.js
+++ local_modules/sensotrend-converter/src/templates/fiphr/index.js
@@ -164,23 +164,23 @@ export class FIPHRDataProcessor extends DataFormatConverter {
       if (entry.value && entry.units) {
          if (entry.units == 'mg/dL') {
             entry.valueMmol = Math.round((entry.value / 18.0156) * 100) / 100;
-            textArray.push("Tulos: " + entry.valueMmol + " mmol/l");
+            textArray.push("Tulos: " + (entry.valueMmol ? entry.valueMmol.toFixed(2) : "?") + " mmol/l");
 
             if (entry.delta) {
                entry.deltaMmol = Math.round((entry.delta / 18.0156) * 100) / 100;
-               textArray.push("Muutos: " + entry.deltaMmol + " mmol/l");
+               textArray.push("Muutos: " + (entry.deltaMmol ? entry.deltaMmol.toFixed(2) : "?") + " mmol/l");
             }
          } else {
             entry.valueMmol = entry.value;
-            textArray.push("Tulos: " + entry.valueMmol) + " mmol/l";
+            textArray.push("Tulos: " + (entry.valueMmol ? entry.valueMmol.toFixed(1) : "?") + " mmol/l");
 
             if (entry.delta) {
                entry.deltaMmol = entry.delta;
-               textArray.push("Muutos: " + entry.deltaMmol + " mmol/l");
+               textArray.push("Muutos: " + (entry.deltaMmol ? entry.deltaMmol.toFixed(1) : "?") + " mmol/l");
             }
          }
       }
-
+      /*
       if (entry.direction) {
          textArray.push("Suunta: " + entry.direction);
       }
@@ -188,6 +188,7 @@ export class FIPHRDataProcessor extends DataFormatConverter {
       if (entry.noise) {
          textArray.push("Mittauslaatu: " + entry.noise);
       }
+      */
 
       entry._statusMessage = textArray.join('<br />');
 
