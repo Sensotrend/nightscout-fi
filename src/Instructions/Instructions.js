772fc1fc4a75a11031c60dd021973a7abcb53bf2
Mikael Rinnetmäki <mikael@sensotrend.com>
2020-06-09 14:37:21 +0300

    Update instructions with proper screen captures

--- src/Instructions/Instructions.js
+++ src/Instructions/Instructions.js
@@ -193,7 +193,7 @@ class Instructions extends Component {
             </p>
             <h3 id="onboarding">Palvelun käyttöönotto</h3>
             <p>
-              Palvelun käyttöönottoon liittyy 5 vaihetta:
+              Palvelun käyttöönottoon liittyy 3 vaihetta:
             </p>
             <ol>
               <li id="signin">
@@ -213,7 +213,7 @@ class Instructions extends Component {
               <li>
                 <figure>
                   <figcaption>
-                    Salli Nightscout.fi-sovelluksen lukea ja kirjoittaa tietoja puolestasi
+                    Salli Sensotrend Connect -sovelluksen lukea ja kirjoittaa tietoja puolestasi
                     Omatietovarantoon.
                     </figcaption>
                   <img alt="Ruutukaappaus Omatietovarannon luvituksesta" src={consentImage} />
@@ -229,15 +229,10 @@ class Instructions extends Component {
                   <img alt="Ruutukaappaus sähköpostiosoitteen kysymisestä" src={emailRegImage} />
                 </figure>
               </li>
-              <li>
-                <figure>
-                  <figcaption>
-                    Vahvistettuasi sähköpostiosoitteesi tunnuksesi on aktivoitu
-                  </figcaption>
-                  <img alt="Ruutukaappaus palvelun näkymästä" src={credentialsImage} />
-                </figure>
-              </li>
             </ol>
+            <p>
+              Vahvistettuasi sähköpostiosoitteesi tunnuksesi on aktivoitu.
+            </p>
             <p>
               Voit käyttää luomaasi käyttäjätunnusta ja salasanaa <a
               href="#SensotrendUploader">Sensotrend Uploader</a> - ja <a
