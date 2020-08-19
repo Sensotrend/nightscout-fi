bfa34aa815a2851cda7cf36082232142d540bcf2
Mikael Rinnetmäki <mikael@sensotrend.com>
2020-08-19 21:08:33 +0300

    Continue working on enabling SSL on localhost

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,1 +1,1 @@
-export const server = process.env.NODE_ENV === 'development' ? 'http://localhost:1300' : '';
+export const server = process.env.NODE_ENV === 'development' ? 'https://localhost:1300' : '';
