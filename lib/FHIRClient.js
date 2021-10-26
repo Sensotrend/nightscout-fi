Sensotrend/nightscout-fi

commit b106f1dcd67f386aed26daca9fa794a87ce06f7c

Use documented options for https client auth
```
 DeprecationWarning: Got: "options.key" was never documented, please use "options.https.key"
at process.emitWarning (internal/process/warning.js:132:11)
at process.<anonymous> (/home/jelastic/ROOT/node_modules/esm/esm.js:1:289642)
at process.<anonymous> (/home/jelastic/ROOT/node_modules/esm/esm.js:1:289518)
at process.<anonymous> (/home/jelastic/ROOT/node_modules/esm/esm.js:1:284879)
at Object.apply (/home/jelastic/ROOT/node_modules/esm/esm.js:1:199341)
at Object.exports.default (/home/jelastic/ROOT/node_modules/got/dist/source/utils/deprecation-warning.js:10:13)
at normalizeArguments (/home/jelastic/ROOT/node_modules/got/dist/source/core/index.js:661:42)
at got (/home/jelastic/ROOT/node_modules/got/dist/source/create.js:112:39)
at Client.request (/home/jelastic/ROOT/node_modules/openid-client/lib/helpers/request.js:53:10)
at Client.authenticatedPost (/home/jelastic/ROOT/node_modules/openid-client/lib/helpers/client.js:154:18)
at runMicrotasks (<anonymous>)
at processTicksAndRejections (internal/process/task_queues.js:97:5)
at async Client.grant (/home/jelastic/ROOT/node_modules/openid-client/lib/client.js:1228:22)
(node:6656) DeprecationWarning: Got: "options.cert" was never documented, please use "options.https.certificate"
at process.emitWarning (internal/process/warning.js:132:11)
at process.<anonymous> (/home/jelastic/ROOT/node_modules/esm/esm.js:1:289642)
at process.<anonymous> (/home/jelastic/ROOT/node_modules/esm/esm.js:1:289518)
at process.<anonymous> (/home/jelastic/ROOT/node_modules/esm/esm.js:1:284879)
at Object.apply (/home/jelastic/ROOT/node_modules/esm/esm.js:1:199341)
at Object.exports.default (/home/jelastic/ROOT/node_modules/got/dist/source/utils/deprecation-warning.js:10:13)
at normalizeArguments (/home/jelastic/ROOT/node_modules/got/dist/source/core/index.js:664:42)
at got (/home/jelastic/ROOT/node_modules/got/dist/source/create.js:112:39)

lib/FHIRClient.js
@@ -14,8 +14,8 @@ function FHIRClient(URL, { patient, bearertoken, env } = {}) {
    const converterName = process.env.CONVERTER_NAME || 'Sensotrend Connect';
 
    if (env && env.https_certificate) {
-      options.cert = env.https_certificate;
-      options.key = env.https_privateKey;
+      options.https.certificate = env.https_certificate;
+      options.https.key = env.https_privateKey;
    }
 
    if (process.env.HTTP_BASIC_AUTH) {

commit ef3d3272eb2d18b2237cbbede1446949a4f26856

Fix syntax for https cert options
Broken in b106f1dcd67f386aed26daca9fa794a87ce06f7c

lib/FHIRClient.js
@@ -14,8 +14,10 @@ function FHIRClient(URL, { patient, bearertoken, env } = {}) {
    const converterName = process.env.CONVERTER_NAME || 'Sensotrend Connect';
 
    if (env && env.https_certificate) {
-      options.https.certificate = env.https_certificate;
-      options.https.key = env.https_privateKey;
+      options.https = {
+         certificate: env.https_certificate,
+         key: env.https_privateKey,
+      }
    }
 
    if (process.env.HTTP_BASIC_AUTH) {